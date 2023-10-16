21d653c217b8defde46cf23ab770fdeadcc1f518
pizzawarrior <i.norstad@gmail.com>
2023-10-16 15:10:49 -0700

    lets have some hats

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    closet_name = models.CharField(max_length=200)
+
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=100)
+    style_name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    photo_url = models.URLField(null=True)
+
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.CASCADE,
+    )
+    # def get_api_url(self):
+    #     return reverse("api_show_conference", kwargs={"id": self.id})
+
+    def __str__(self):
+        return self.name
