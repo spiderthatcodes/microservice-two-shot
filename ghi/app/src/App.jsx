fee26bd1f6171257691ed7c9d343c08d1315dc25
spiderthatcodes <jessicaldickerson@outlook.com>
2023-10-17 09:07:07 -0700

    done

--- ghi/app/src/App.jsx
+++ ghi/app/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
+import MainPage from './components/MainPage';
+import Nav from './components/Nav';
+import ListShoes from './components/ListShoes';
+import CreateShoe from './components/CreateShoe';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path='list-shoes' element={<ListShoes />} />
+          <Route path='create-shoe' element={<CreateShoe />} />
         </Routes>
       </div>
     </BrowserRouter>
